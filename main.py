ad2331cc80164fe49dca6956909ae09519f14ab7
Юлия <coroleva.yu@gmail.com>
2024-09-07 17:51:16 +0300

    add main
    Добавила main.py В этом файле 3 функции для вычислений. Все функции пустые.

---
+++ main.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    pass
+
+import math
+
+def circle_area(radius):
+    pass
+
+
+def average(numbers):
+    pass
+
+print(factorial(5), circle_area(5),average(numbers=[10,20,30,40]))  # Output: 120
